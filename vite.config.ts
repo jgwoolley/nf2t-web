cc89a920f4ff8cf03b4c87520ccdd4a1ee7fffba
James W <26191568+jgwoolley@users.noreply.github.com>
2024-01-05 21:15:51 -0500

    Added buildinfo plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,71 @@
 import { defineConfig } from 'vite'
+import path from 'node:path';
+import fs from 'node:fs';
+import child_process from 'child_process';
 import react from '@vitejs/plugin-react'
 
-const jobId = process.env.GITHUB_JOB;
+const gitShowPlaceholders = [
+  "H", "h",
+  "T", "t",
+  "P", "p",
+  "an", "aN",
+  "ae", "aE",
+  "al", "aL",
+  "ad", "aD",
+  "ar",
+  "at",
+  "ai", "aI",
+  "as",
+  "ah",
+  "cn", "cN",
+  "ce", "cE",
+  "cl", "cL",
+  "cd", "cD",
+  "cr",
+  "ct",
+  "ci", "cI",
+  "cs",
+  "ch",
+  "d", "D",
+  "f",
+]
 
-let base = undefined;
-const repository = process.env.GITHUB_REPOSITORY;
-if (repository !== undefined) {
-  const repositorySplit = repository.split("/");
-  base = repositorySplit.length <= 0 ? undefined: repositorySplit[1];
+function calculateGithub() {
+  const jobId = process.env.GITHUB_JOB;
+  let base = undefined;
+  const repository = process.env.GITHUB_REPOSITORY;
+  if (repository !== undefined) {
+    const repositorySplit = repository.split("/");
+    base = repositorySplit.length <= 0 ? undefined: repositorySplit[1];
+  }
+
+  return {
+    jobId, 
+    repository, 
+    base,
+  };
 }
 
-console.log({jobId, repository, base});
+const { jobId, repository, base } = calculateGithub()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: base,
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: "buildinfo",
+      closeBundle: () => {
+        const gitlog_path = path.join(__dirname, "dist", "buildinfo.json");
+        const gitlog_text = child_process.execSync(`git log -1 --pretty=format:\'{${gitShowPlaceholders.map(x => `\"${x}\": \"%${x}\"`).join(", ")}}\'`)
+        const gitlog = JSON.parse(gitlog_text.toString());
+        const buildinfo = {
+          git: gitlog,
+          github: calculateGithub(),
+        }
+
+        fs.writeFileSync(gitlog_path, JSON.stringify(buildinfo));
+      },
+    }
+  ],
 })
